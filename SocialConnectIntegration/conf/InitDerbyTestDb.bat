077b81f3094d7b6e2b5712ce9293a7e3248cc97d
SaschaRodekamp <sascha.rodekamp@gmail.com>
2012-07-11 08:48:39 +0200

    - Improve integration testing. For each test run a new derby db will be created and filled with demo data.

--- SocialConnectIntegration/conf/InitDerbyTestDb.bat
+++ SocialConnectIntegration/conf/InitDerbyTestDb.bat
@@ -1,9 +1,10 @@
-java -version
+@echo off
 
-SET DERBY_HOME=derby
-SET derby.system.home=../target/runtime/derby
-SET DERBY_OPTS=-Dderby.system.home=../target/runtime/derby
+SET DERBY_HOME=conf\derby
+SET derby.system.home=target\runtime\derby
+SET DERBY_OPTS=-Dderby.system.home=target\runtime\derby
 
-CALL %DERBY_HOME%\ij.bat db_scripts/Derby_DDL.sql
-CALL %DERBY_HOME%\ij.bat db_scripts/Derby_DML_MasterData.sql
-CALL %DERBY_HOME%\ij.bat db_scripts/Derby_DML_DemoData.sql
+
+CALL %DERBY_HOME%\ij.bat conf\db_scripts\Derby_DDL.sql
+CALL %DERBY_HOME%\ij.bat conf\db_scripts\Derby_DML_MasterData.sql
+CALL %DERBY_HOME%\ij.bat conf\db_scripts\Derby_DML_DemoData.sql
