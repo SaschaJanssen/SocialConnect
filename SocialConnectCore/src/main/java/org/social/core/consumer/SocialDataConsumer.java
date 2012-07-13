9af1cd1a939d13a2c0230486e3e9451923727f8a
sascharodekamp <sascha.rodekamp@gmail.com>
2012-07-13 19:10:27 +0200

    - remove wrong comment

--- SocialConnectCore/src/main/java/org/social/core/consumer/SocialDataConsumer.java
+++ SocialConnectCore/src/main/java/org/social/core/consumer/SocialDataConsumer.java
@@ -69,8 +69,8 @@ public class SocialDataConsumer {
 						+ " data consumer and filtering thread.");
 			}
 
-//			List<Messages> receivedMsgList = networkConnection.fetchMessages();
-			List<Messages> receivedMsgList = new ArrayList<Messages>();
+			List<Messages> receivedMsgList = networkConnection.fetchMessages();
+//			List<Messages> receivedMsgList = new ArrayList<Messages>();
 
 			if (logger.isDebugEnabled()) {
 				logger.debug("Found " + receivedMsgList.size() + " Messages from Network: " + networkConnection.getClass().getName());
