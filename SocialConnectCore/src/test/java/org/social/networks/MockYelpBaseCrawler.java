d074da61dd18128094726f348f884e93e097d6d7
SaschaRodekamp <sascha.rodekamp@gmail.com>
2012-08-01 15:29:16 +0200

    - add unit tests for yelp and open table

---
+++ SocialConnectCore/src/test/java/org/social/networks/MockYelpBaseCrawler.java
@@ -0,0 +1,18 @@
+package org.social.networks;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.social.core.network.crawler.BaseCrawler;
+
+public class MockYelpBaseCrawler implements BaseCrawler{
+
+	@Override
+	public Document crwal(String url) throws IOException {
+		File fi = new File("src/test/resources/YelpVapianoTest_WithoutPagination.html");
+		return Jsoup.parse(fi, "UTF-8");
+	}
+
+}
