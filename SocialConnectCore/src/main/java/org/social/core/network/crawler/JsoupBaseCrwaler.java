9921bba51c3b5df1a373eea53a411040bd15b362
SaschaRodekamp <sascha.rodekamp@gmail.com>
2012-08-02 14:37:03 +0200

    - update crawler data fetching, when a timeout exception is thrown try four times again to get the data before throwing an exception back

--- SocialConnectCore/src/main/java/org/social/core/network/crawler/JsoupBaseCrwaler.java
+++ SocialConnectCore/src/main/java/org/social/core/network/crawler/JsoupBaseCrwaler.java
@@ -7,7 +7,7 @@ import org.jsoup.nodes.Document;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class JsoupBaseCrwaler implements BaseCrawler{
+public class JsoupBaseCrwaler implements BaseCrawler {
 
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
@@ -17,7 +17,22 @@ public class JsoupBaseCrwaler implements BaseCrawler{
 			logger.debug("Start crawling data from: " + url);
 		}
 
-		return Jsoup.connect(url).timeout(5000).get();
+		Exception ex = null;
+		int maxTriesToGetRemoteData = 4;
+		int tries = 0;
+		while (tries < maxTriesToGetRemoteData) {
+			try {
+				return Jsoup.connect(url).timeout(5000).get();
+			} catch (IOException e) {
+				ex = e;
+				if (logger.isWarnEnabled()) {
+					logger.warn("Got a" + e.getMessage() + " Exception, try a again to fetch data from remote address.");
+				}
+				tries++;
+			}
+		}
+
+		throw new IOException("After the " + maxTriesToGetRemoteData + " run, gave up on fatching data from remote url " + url, ex);
 	}
 
 }
